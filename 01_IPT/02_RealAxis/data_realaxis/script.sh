7d695adc583387fa76dd50592904b6fb03998644
Adriano Amaricci <adriano.amaricci@gmail.com>
2019-04-16 10:15:05 +0200

    v0.3 UPDATE
    - 01_Matsubara: added results for the ImSigma. Added the inputFILE in git archive. Added xmgrace .agr sources in data_matsubara
    - 02_RealAxis: added xmgrace sources. Added the inputFILE in git archive.

--- 01_IPT/02_RealAxis/data_realaxis/script.sh
+++
@@ -1,9 +0,0 @@
-while read U;do
-    DIR=U$U;
-    mkdir -p $DIR;
-    cp -v *restart inputIPT.conf $DIR/;
-    cd $DIR/
-    ipt_hm_real uloc=$U |tee LOG.out
-    cp *.restart ../
-    cd ../
-done<list_u
